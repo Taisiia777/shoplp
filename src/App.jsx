da63b849e1abce5b222c2d28ff4914c789cc9c85
Taisiia777 <sporyshev.savelii@gmail.com>
2024-11-18 16:43:03 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,8 @@ import React from "react";
 import Routes from "./Routes";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./styles/App.css"; // Import the CSS for transitions
-import ReactGA from 'react-ga4';
 
 function App() {
-    useEffect(() => {
-        ReactGA.initialize('G-S29PJYNV79'); // Ваш Tracking ID
-        ReactGA.send("pageview");
-      }, []);
     return (
         <Router>
             <Routes />
